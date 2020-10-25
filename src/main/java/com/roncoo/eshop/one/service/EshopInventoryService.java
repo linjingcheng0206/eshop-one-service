8fe1322663cde9edc253b7ff27580b2ac6099fa5
ljc <linjingcheng0206@163.com>
2020-10-25 23:13:39 +0800

    修改bug

--- src/main/java/com/roncoo/eshop/one/service/EshopInventoryService.java
+++ src/main/java/com/roncoo/eshop/one/service/EshopInventoryService.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @FeignClient(value = "eshop-inventory-service",fallback = EshopInventoryServiceFallback.class)
 public interface EshopInventoryService {
 
--- src/main/java/com/roncoo/eshop/one/service/fallback/EshopPriceServiceFallback.java
+++ src/main/java/com/roncoo/eshop/one/service/fallback/EshopPriceServiceFallback.java
@@ -1,12 +1,15 @@
 package com.roncoo.eshop.one.service.fallback;
 
-import com.roncoo.eshop.one.service.EshopInventoryService;
+import com.roncoo.eshop.one.service.EshopPriceService;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author linjingcheng
  * @data 2020/10/25
  */
-public class EshopPriceServiceFallback implements EshopInventoryService {
+@Component
+
+public class EshopPriceServiceFallback implements EshopPriceService {
     @Override
     public String findByProductId(Long productId) {
         return "降级价格数据";
